4549056fb2cc39bf056781d6b03261011063968d
Suraj-09 <33708841+Suraj-09@users.noreply.github.com>
2023-10-20 18:47:10 -0400

    comment recipe generation code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from recipe_generator  import generation_function
+# from recipe_generator  import generation_function
 
 app = Flask(__name__)
 
@@ -7,16 +7,16 @@ app = Flask(__name__)
 def hello():
     return 'Welcome to the Cook It Up API!'
 
-@app.route('/test', methods=["GET"])
-def test():
-    ingredients = request.json['ingredients']
+# @app.route('/test', methods=["GET"])
+# def test():
+#     ingredients = request.json['ingredients']
 
-    if ingredients is None:
-        return (
-            jsonify({"error": "Missing 'ingredients' parameter in the GET request."}),
-            400,
-        )
+#     if ingredients is None:
+#         return (
+#             jsonify({"error": "Missing 'ingredients' parameter in the GET request."}),
+#             400,
+#         )
     
-    response = generation_function(ingredients)
+#     response = generation_function(ingredients)
 
-    return jsonify({"recipe": response})
+#     return jsonify({"recipe": response})
